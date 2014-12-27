96af04feb7eddb055a309553bf889f1526154afb
Geert Eltink <xtreamwayz@gmail.com>
2014-12-27 14:29:06 +0100

    First commit

---
+++ footer.php
@@ -0,0 +1,19 @@
+        </div>
+    </div>
+
+    <footer id="colophon" class="footer" role="contentinfo">
+        <div class="container footer__site">
+            <div class="row">
+                <div class="col-md-6">
+                    LEFT
+                </div>
+                <div class="col-md-6 text-right">
+                    Developed by <a href="http://twitter.com/xtreamwayz">@xtreamwayz</a>
+                </div>
+            </div>
+        </div>
+    </footer><!-- #colophon -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title><?php wp_title(); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="stylesheet" href="//www.google.com/fonts#ReviewPlace:refine/Collection:Noto+Serif:400,700,400italic,700italic" type="text/css" media="all" />
+	<link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/css/stylesheet.css" type="text/css" media="all" />
+
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<?php if (is_singular() && get_option('thread_comments')) { wp_enqueue_script('comment-reply'); } ?>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<a class="skip-link sr-only" href="#content">Skip to content</a>
+
+	<header class="header">
+		HEADER
+	</header>
+
+    <div id="content" class="content">
+        <div class="container">
