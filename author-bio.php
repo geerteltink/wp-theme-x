1dac8750309bf9877f646fee0e10d4eec3cc3ffd
Geert Eltink <xtreamwayz@gmail.com>
2014-12-29 21:31:47 +0100

    Limit author avatar size a bit more

--- author-bio.php
+++ author-bio.php
@@ -3,13 +3,13 @@
 	<div class="media">
 		<div class="media-body">
 			<p class="post-author__name"><?php echo get_the_author(); ?></p>
-			<p class="post-author__desc"><?php the_author_meta( 'description' ); ?></p>
-			<a class="post-author__link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-				<?php printf( __( 'View all posts by %s', 'twentyfifteen' ), get_the_author() ); ?>
+			<p class="post-author__desc"><?php the_author_meta('description'); ?></p>
+			<a class="post-author__link" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
+				<?php printf( __('View all posts by %s', 'twentyfifteen'), get_the_author()); ?>
 			</a>
 		</div>
-		<div class="media-right">
-			<?php echo get_avatar( get_the_author_meta( 'user_email' ), 256); ?>
+		<div class="media-right post-author__avatar">
+			<?php echo get_avatar(get_the_author_meta('user_email'), 96); ?>
 		</div>
 	</div>
 </div>
