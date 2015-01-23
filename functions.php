a89d0752aab235e3b83b8e97b94b725812be3ebe
Geert Eltink <xtreamwayz@gmail.com>
2015-01-23 21:27:34 +0100

    Add automatic-feed-links support

--- functions.php
+++ functions.php
@@ -21,28 +21,22 @@ function themex_setup()
 	set_post_thumbnail_size(640, 360, true);
 
 	add_theme_support('title-tag');
+	add_theme_support('automatic-feed-links');
+
+	// Use html5
+	add_theme_support('html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	));
+
+	//See: https://codex.wordpress.org/Post_Formats
+	add_theme_support('post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+	));
 
 	// This theme uses wp_nav_menu() in two locations.
 	register_nav_menus(array(
 		'primary' => __('Primary Menu',      'themex'),
 		'social'  => __('Social Links Menu', 'themex'),
-	));
-
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support('html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-	));
-
-	/*
-	 * Enable support for Post Formats.
-	 *
-	 * See: https://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support('post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
 	));
 }
 add_action('after_setup_theme', 'themex_setup');
