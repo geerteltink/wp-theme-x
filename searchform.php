fc882fdc0cf146f67e3833b7e3cabaae5fdec947
Geert Eltink <xtreamwayz@gmail.com>
2015-01-25 14:45:39 +0100

    A submit button which actually submits the form might be handy

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
     <div class="input-group">
         <input type="text" class="form-control" name="s" value="<?php echo get_search_query(); ?>" id="s" placeholder="<?php echo esc_attr_x('Search ...', 'placeholder') ?>">
         <span class="input-group-btn">
-            <button class="btn btn-primary" id="searchsubmit" type="button">
+            <button class="btn btn-primary" id="searchsubmit" type="submit">
                 <?php echo esc_attr_x('Search', 'submit button'); ?>
             </button>
         </span>
