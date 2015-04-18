ba33a70d4300a97041a8ca8cda0233626804f350
Geert Eltink <xtreamwayz@gmail.com>
2015-04-18 11:46:37 +0200

    Make comment form bootstrap compatible

--- functions.php
+++ functions.php
@@ -166,3 +166,46 @@ function themex_optimize_page_loading()
 	add_action('wp_footer', 'wp_enqueue_scripts', 5);
 }
 add_action('after_setup_theme', 'themex_optimize_page_loading');
+
+/**
+ * Make form fields work with bootstrap
+ */
+function themex_comment_form_fields($fields)
+{
+    $commenter = wp_get_current_commenter();
+
+    $req       = get_option('require_name_email');
+    $aria_req  = ($req ? ' aria-required="true"' : '');
+    $html5     = current_theme_supports('html5', 'comment-form') ? true : false;
+
+    $fields = array(
+        'author' => '<div class="form-group comment-form-author">' . '<label for="author">' . __('Name') . ($req ? ' <span class="required">*</span>' : '') . '</label>' .
+                    '<input class="form-control" id="author" name="author" type="text" value="' . esc_attr($commenter['comment_author']) . '" size="30"' . $aria_req . ' /></div>',
+        'email'  => '<div class="form-group comment-form-email"><label for="email">' . __('Email') . ($req ? ' <span class="required">*</span>' : '') . '</label>' .
+                    '<input class="form-control" id="email" name="email" ' . ($html5 ? 'type="email"' : 'type="text"') . ' value="' . esc_attr($commenter['comment_author_email']) . '" size="30"' . $aria_req . ' /></div>',
+        'url'    => '<div class="form-group comment-form-url"><label for="url">' . __('Website') . '</label> ' .
+                    '<input class="form-control" id="url" name="url" ' . ($html5 ? 'type="url"' : 'type="text"') . ' value="' . esc_attr($commenter['comment_author_url']) . '" size="30" /></div>'
+    );
+
+    // Remove url field
+    unset($fields['url']);
+
+    return $fields;
+}
+add_filter('comment_form_default_fields', 'themex_comment_form_fields');
+
+/**
+ * Make comment form work with bootstrap
+ */
+function themex_comment_form_defaults($args)
+{
+    $args['comment_field'] = '<div class="form-group comment-form-comment">
+            <label for="comment">' . _x('Comment', 'noun') . '</label>
+            <textarea class="form-control" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
+        </div>';
+
+    $args['class_submit'] = 'btn btn-primary'; // since WP 4.1
+
+    return $args;
+}
+add_filter('comment_form_defaults', 'themex_comment_form_defaults');
